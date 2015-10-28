a9cbdfa675e4353868eb0cede8b68e4906075723
Victor Alveflo <victor.alveflo@gmail.com>
2015-10-28 20:58:15 +0100

    Fixes #1

--- chief.ps1
+++ chief.ps1
@@ -32,38 +32,55 @@ param (
   [switch]$IncludeSubDirectories
 )
 
-Function Watch($src, $dest, $includeSubDirectories) {
-  $watcher = New-Object IO.FileSystemWatcher $src -Property @{
-    IncludeSubDirectories = $includeSubDirectories
-    NotifyFilter = [IO.NotifyFilters] 'FileName, LastWrite'
-  }
+Function Watch($SourcePath, $DestPath, $IncludeSubDirectories) {
+	try{
+	  $watcher = New-Object IO.FileSystemWatcher $SourcePath -Property @{
+	    IncludeSubDirectories = $IncludeSubDirectories
+	    NotifyFilter = [IO.NotifyFilters] 'FileName, LastWrite'
+	  }
 
-  $delegate = {
-    $name = $Event.SourceEventArgs.Name
-    $changeType = $Event.SourceEventArgs.ChangeType
-    Write-Host "[$(Get-Date -Format 'HH:mm:ss')]: " -Foreground DarkGray -NoNewLine
-    switch ($changeType)
-    {
-      'Created' { Write-Host "$changeType " -Foreground Green    -NoNewLine }
-      'Changed' { Write-Host "$changeType " -Foreground Cyan     -NoNewLine }
-      'Renamed' { Write-Host "$changeType " -Foreground Yellow   -NoNewLine }
-      'Deleted' { Write-Host "$changeType " -Foreground Red      -NoNewLine }
-    }
-    Write-Host $name -Foreground DarkCyan
-  }
+	  $delegate = {
+	    $name = $Event.SourceEventArgs.Name
+			$path = $Event.SourceEventArgs.FullPath
+	    $changeType = $Event.SourceEventArgs.ChangeType
+			$conf = $Event.MessageData
 
-  $fileCreated  = Register-ObjectEvent $watcher Created -SourceIdentifier FileCreated -Action $delegate
-  $fileChanged  = Register-ObjectEvent $watcher Changed -SourceIdentifier FileChanged -Action $delegate
-  $fileRenamed  = Register-ObjectEvent $watcher Renamed -SourceIdentifier FileRenamed -Action $delegate
-  $fileDeleted  = Register-ObjectEvent $watcher Deleted -SourceIdentifier FileDeleted -Action $delegate
+	    Write-Host "[$(Get-Date -Format 'HH:mm:ss')]: " -Foreground DarkGray -NoNewLine
+	    switch ($changeType)
+	    {
+	      'Created' { Write-Host "$changeType " -Foreground Green    -NoNewLine }
+	      'Changed' { Write-Host "$changeType " -Foreground Cyan     -NoNewLine }
+	      'Renamed' { Write-Host "$changeType " -Foreground Yellow   -NoNewLine }
+	      'Deleted' { Write-Host "$changeType " -Foreground Red      -NoNewLine }
+	    }
+	    Write-Host $path.Replace($conf.src, '') -Foreground DarkCyan -NoNewLine
+	    if ($changeType -ne 'Deleted') {
+				$destinationFile = Join-Path $conf.dest $path.Replace($conf.src, '')
+				$destinationPath = Join-Path $conf.dest $path.Replace($conf.src, '').Replace($name, '')
+				if (-not (Test-Path $destinationPath)) {
+					New-Item -ItemType Directory -Path $destinationPath
+				}
+	      Copy-Item $path $destinationFile
+				Write-Host " (-> $destinationFile)" -Foreground DarkGray -NoNewLine
+	    }
+			Write-Host
+	  }
 
-  try{
+		$config = New-Object PSObject -Property @{src = $SourcePath; dest = $DestPath}
+		Write-Host $config
+	  $fileCreated  = Register-ObjectEvent $watcher Created -SourceIdentifier FileCreated -Action $delegate -MessageData $config
+	  $fileChanged  = Register-ObjectEvent $watcher Changed -SourceIdentifier FileChanged -Action $delegate -MessageData $config
+	  $fileRenamed  = Register-ObjectEvent $watcher Renamed -SourceIdentifier FileRenamed -Action $delegate -MessageData $config
+	  $fileDeleted  = Register-ObjectEvent $watcher Deleted -SourceIdentifier FileDeleted -Action $delegate -MessageData $config
+
+
     cls
     Write-Host "`n`n`tAhoy!" -Foreground Green -NoNewLine
-    Write-Host "`n`n`t`tI'm watchin' your file system, chief!`n`t`tYou know the drill, press Ctrl+C to cancel.`n`n"
+    Write-Host "`n`n`tI'm watchin' your file system, chief!`n`tYou know the drill, press Ctrl+C to cancel.`n`n"
     while($true){}
-  }
-  finally {
+  } catch {
+		throw $_.Exception
+	} finally {
     Write-Host "`n`n`tUnregistering events..."
     Unregister-Event FileCreated
     Unregister-Event FileChanged
